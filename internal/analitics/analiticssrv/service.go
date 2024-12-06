e786c17d2772f622d287bbcef59b574ea5ce7978
Abraxas-365 <luisfmiranda8@gmail.com>
2024-12-06 16:42:43 -0500

    test

--- internal/analitics/analiticssrv/service.go
+++ internal/analitics/analiticssrv/service.go
@@ -18,17 +18,17 @@ func NewService(repo analitics.Repository) *Service {
 func (s Service) GetAllAnalitics(ctx context.Context, startDate *time.Time, endDate *time.Time) ([]analitics.Statistic, error) {
 	var allAnalitics []analitics.Statistic
 
-	// interactions, err := s.repo.GetInteractions(ctx, startDate, endDate)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// allAnalitics = append(allAnalitics, *interactions)
-
-	data, err := s.repo.GetMostConsultedData(ctx, startDate, endDate)
+	interactions, err := s.repo.GetInteractions(ctx, startDate, endDate)
 	if err != nil {
 		return nil, err
 	}
-	allAnalitics = append(allAnalitics, *data)
+	allAnalitics = append(allAnalitics, *interactions)
+
+	// data, err := s.repo.GetMostConsultedData(ctx, startDate, endDate)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// allAnalitics = append(allAnalitics, *data)
 
 	users, err := s.repo.GetTotalUsers(ctx, startDate, endDate)
 	if err != nil {
